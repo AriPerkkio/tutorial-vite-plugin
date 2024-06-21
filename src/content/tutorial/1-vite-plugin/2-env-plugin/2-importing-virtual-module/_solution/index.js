eff66e103b147f035f2ca392506175bb169cf9ee
Ari Perkkiö <ari.perkkio@gmail.com>
2024-06-21 15:34:48 +0300

    feat: importing virtual module

---
+++ src/content/tutorial/1-vite-plugin/2-env-plugin/2-importing-virtual-module/_solution/index.js
@@ -0,0 +1,4 @@
+import env from "virtual:tutorial-env";
+const output = "Initial output";
+
+export default output;
---
+++ src/templates/env-plugin/main.js
@@ -0,0 +1,26 @@
+import "./style.css";
+
+const ID = "vite-plugin-content";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <h1>Hello Vite Plugin tutorial!</h1>
+    <p>Loaded Vite plugins: ${window.TUTORIAL_LOADED_VITE_PLUGINS || "none"}</p>
+    <pre id="${ID}"></pre>
+  </div>
+`;
+
+import("./index.js")
+  .then((mod) => {
+    document.getElementById(ID).textContent = JSON.stringify(
+      mod.default,
+      null,
+      2
+    );
+  })
+  .catch((error) => {
+    const element = document.getElementById(ID);
+    const message = `Failed to load index.js: "${error instanceof Error ? error.message : String(error)}" ❌`;
+    element.textContent = message;
+    element.classList.add("error");
+  });
